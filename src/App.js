3ff54d70003e4d19399dffb5312a7983d339d5a6
Malcom Otieno <malcolmotieno16@gmail.com>
2024-11-25 20:51:43 +0300

    new words!

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const App = () => {
     <div>
       <h1>Hello, It's my first React App!</h1>
        <h2>Happy Coding!!</h2>
+       <h3>The First of many great things to come!
+       </h3>
     </div>
   );
 };
